52c76e3c216bdb0e3d93556e9905733883130ccc
saifkhan7865 <60229505+saifkhan7865@users.noreply.github.com>
2021-04-09 22:54:04 +0530

    added variables py file

---
+++ variables.py
@@ -0,0 +1,24 @@
+# x = 15
+# y = 25
+# name = "Mohammed Khan"
+# height = 2.3
+# 
+# addvar= x + y
+
+# print(f"The addition of x and y variable is = {addvar}")
+# print(f"My name is {name}")
+
+#to create a variable PIE and store 3.14
+
+# to create radius and store any radius
+
+# store the area of the circle in the variable called AREA
+
+
+PIE = 3.14
+radius = 7
+
+AREA = PIE * radius * radius
+
+print(f'''The radius of the circle is = {radius} 
+and the area of the circle is = {AREA}''')
